bb7eef848eda57a1b09c585eabcbab86c743214b
nkobaidze <nika.kobaidze@gmail.com>
2022-09-19 17:08:55 +0400

    [FIX] readme

--- meeting-2/homework-1/1-homework-solution.js
+++ meeting-2/homework-1/1-homework-solution.js
@@ -5,28 +5,35 @@ function Car(make, model, year){
     this.owners = [];
     
     this.getCarInfo = () => {
+        // DONE
         return this.make + " " + this.model + " released in " + this.year; 
     }
     
     this.addOwner = (owner) => {
+        // DONE
         this.owners.push(owner);
     }
     
     this.removeOwner = (owner) => {
-        this.owners.filter(item => item !== owner)
+        // DONE
+        // Simple filter
+        this.owners = this.owners.filter(item => item.name !== owner.name && item.surname !== owner.surname);
     }
     
     this.getOwnersCount = () => {
+        // DONE
         return this.owners.length;
     }
     
     this.getOwnerNames = () => {
+        // DONE
         return this.owners.map(person => person.fullName());
     }
     
     this.getFullInfo = () => {
+        // DONE
         // Tesla Model S from 2015. 2 person owns this car. These are - Daniel Barbakadze, Elon Musk.
-        return "FULL INFO";
+        return this.make + " " + this.model + " from " +  this.year + ". " + this.getOwnersCount() + " person owns this car. These are - " + this.getOwnerNames().join(' ,');
     }
 }
 
@@ -39,35 +46,73 @@ function Person(name, surname, age, gender, cars = []){
     this.cars = cars;
     
     this.fullName = () => {
+        // DONE
         return this.name + " " + this.surname;
     }
     
     this.countCars = () => {
+        // DONE
         return this.cars.length;
     }
     
     this.buysCar = (car) => {
-        this.cars.push(car)
-        car.addOwner(this)
+        // DONE
+        this.cars.push(car);
+        car.addOwner(this);
     }
     
     this.sellsCar = (car) => {
-        return "REMOVE CAR";
+        // DONE
+        car.removeOwner(this);
+        // Simple Filter
+        this.cars = this.cars.filter(item => item.make !== car.make && item.model !== car.model);
     }
     
     this.getAllCarsInfo = () => {
+        // DONE
         // Daniel owns these cars: Tesla Model S released in 2015, BMW M2 released in 2019.
+        return this.name + " owns these cars: " + this.cars.map(car => car.getCarInfo()).join(', ')
     }
 }
 
 
 
 
+let daniel916 = new Person("Daniel", "Barbakadze", 21, "M", []);
+let ilona = new Person("Elon", "Musk", 30, "M");
 
+let duti_picoti = new Car("BMW", "525", "1999");
+let stodevianosto = new Car("Mercedes", "E190", 1991);
 
+daniel916.buysCar(duti_picoti); // adds passed car
+daniel916.buysCar(stodevianosto); // adds passed car
+daniel916.sellsCar(duti_picoti); // removes passed car
 
-let car = new Car("Mercedes", "E190", 1991);
-console.log(car.getCarInfo())
+ilona.buysCar(stodevianosto); // adds passed car
+ilona.buysCar(duti_picoti); // adds passed car
 
-
-
+console.log({
+  daniel: {
+    fullName: daniel916.fullName(),
+    countCars: daniel916.countCars(),
+    getAllCarsInfo: daniel916.getAllCarsInfo(),
+  }
+  ,
+  elon: {
+    fullName: ilona.fullName(),
+    countCars: ilona.countCars(),
+    getAllCarsInfo: ilona.getAllCarsInfo(),
+  },
+  duti_picoti: {
+    getOwnersCount: duti_picoti.getOwnersCount(),
+    getOwnerNames: duti_picoti.getOwnerNames(),
+    getFullInfo: duti_picoti.getFullInfo(),
+    getCarInfo: duti_picoti.getCarInfo(),
+  },
+  stodevianosto: {
+    getOwnersCount: stodevianosto.getOwnersCount(),
+    getOwnerNames: stodevianosto.getOwnerNames(),
+    getFullInfo: stodevianosto.getFullInfo(),
+    getCarInfo: stodevianosto.getCarInfo(),
+  },
+});
