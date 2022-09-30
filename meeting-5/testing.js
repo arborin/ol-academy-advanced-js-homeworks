5a3b5f0fb50b1b04615d15d9e3f15bcb8bef1e74
nkobaidze <nika.kobaidze@gmail.com>
2022-09-30 12:47:53 +0400

    [ADD] promise

--- meeting-5/testing.js
+++ meeting-5/testing.js
@@ -1,3 +1,5 @@
+// CALLBACK EXAMPLE
+// =====================================================================
 function counting(number, alert){
     
     for(let i=0; i<number; i++){
@@ -12,5 +14,38 @@ function alert(){
     console.log("job done!")
 }
 
+// counting(5000, alert);
 
-counting(5000, alert);
+
+// =====================================================================
+
+
+function printString(string){
+    setTimeout(
+        () => {
+            for(let i=0; i<5; i++){
+                console.log(string);
+            } 
+        },
+        Math.floor(Math.random() * 100) +1
+    );
+}
+
+function printAll(){
+    printString("A");
+    printString("B");
+    printString("C");
+}
+
+printAll();
+  
+
+// PROMISES
+function printString(string){
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log(string);
+            res();
+        }, Math.floor(Math.random()*100) +1 );
+    })
+}
