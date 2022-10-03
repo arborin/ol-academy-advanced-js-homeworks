154066e6822e43dd616c01889d9cec00214153d2
arborin <nika.kobaidze@gmail.com>
2022-10-03 23:05:24 +0400

    [ADD] promices

--- meeting-5/testing/js_nuggets_callback_hell.js
+++ meeting-5/testing/js_nuggets_callback_hell.js
@@ -1,17 +1,17 @@
 function run(){
     setTimeout(() => {
         console.log("first");
+        
         setTimeout(() => {
             console.log("second");
+            
             setTimeout(() => {
                 console.log("third");
             }, 2000);
+            
         }, 3000);
+        
     }, 1000);
-    
-    
-    
-   
 }
 
 run()
