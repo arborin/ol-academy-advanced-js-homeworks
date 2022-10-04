c09c3bbaee091ddbda9ad1b754c334c7fca89ce6
arborin <nika.kobaidze@gmail.com>
2022-10-05 00:05:10 +0400

    [add] event loop

---
+++ meeting-6/event_loop.js
@@ -0,0 +1,10 @@
+/*
+Concurrency model and the Event Loop
+
+JavaScript has a concurrency model based on an event loop, 
+which is responsible for executing the code, 
+collecting and processing events, and executing queued sub-tasks. 
+
+This model is quite different 
+from models in other languages like C and Java.
+*/
