6b85a8b57f3771f947d4184ae78e9310247fe938
NikaKobaidze <nika.kobaidze@gmail.com>
2022-10-10 19:15:44 +0000

    [ADD] git ignore

--- meeting-8/learning/main.js
+++ meeting-8/learning/main.js
@@ -10,3 +10,11 @@ console.log(moment(myDate).format('LL'));
 
 let myList = [1,2,3,3,3,1,1,2,2,23,4,5,5,5,6,6,6];
 console.log(uniq(myList).length);
+
+const _ = require('lodash');
+
+const numbers = [32,543,123,45];
+
+_.each(numbers, function(number, i){
+  console.log(number);
+});
